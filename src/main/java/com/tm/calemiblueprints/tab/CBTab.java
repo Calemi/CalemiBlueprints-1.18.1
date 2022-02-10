b0efa5cbca2f30e89fd5fd0524999645490b8bca
Calemi <lemzsteamerson@gmail.com>
2022-02-09 21:31:50 -0500

    Updated Tab Icon

--- src/main/java/com/tm/calemiblueprints/tab/CBTab.java
+++ src/main/java/com/tm/calemiblueprints/tab/CBTab.java
@@ -1,9 +1,9 @@
 package com.tm.calemiblueprints.tab;
 
+import com.tm.calemiblueprints.init.InitItems;
 import com.tm.calemiblueprints.main.CBReference;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 
 public class CBTab extends CreativeModeTab {
 
@@ -13,6 +13,6 @@ public class CBTab extends CreativeModeTab {
 
     @Override
     public ItemStack makeIcon () {
-        return new ItemStack(Items.APPLE);
+        return new ItemStack(InitItems.BLUEPRINT_ITEM.get());
     }
 }
